36fceeeffe65062920c826efdcd2a40cde5d1909
VJNap <vjnapolitano1364@gmail.com>
2019-09-14 19:08:47 -0500

    VN - Updated routes js files with the beginnings of get/post and path functions.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,5 +11,10 @@ var userData = require("../data/friends");
 // ===============================================================================
 
 module.exports = function(app) {
-
+    app.get('api/friends', function(req,res) {
+        res.json(userData);
+    });
+    app.post('api/friends', function(req,res) {
+        
+    });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,6 +9,11 @@ var path = require("path");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
-
+module.exports = function (app) {
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 };
